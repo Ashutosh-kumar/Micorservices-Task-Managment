576b306434c50a70afbba747f36a86fded57910e
Ashutosh kumar <ashutosh.kp@gmail.com>
2020-12-30 18:36:28 +0000

    Add files via upload

---
+++ discovery-server-eureka/src/test/java/com/ashutoku/discovery/DiscoveryServerEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashutoku.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
