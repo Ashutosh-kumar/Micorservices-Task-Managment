5b9258cb8a1a945b06813032969f2fe2baceb03a
Ashutosh kumar <ashutosh.kp@gmail.com>
2020-12-30 18:28:57 +0000

    Add files via upload

---
+++ api-gateway/src/test/java/api/gateway/services/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package api.gateway.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
